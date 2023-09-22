d9fb815601d7a8be820a736562669f1185af3e18
Arash Fallahi <ali.fallahi@avenga.com>
2023-09-22 23:43:55 +0200

    improve loop

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import { Canvas } from "./js/canvas.js";
 import { Population } from "./collections/population.collection.js";
 
+const roundRepeat = 10;
 const canvasSize = 500;
 const gridSize = 10;
 const initialPopulation = 10;
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
 const canvas = new Canvas();
-const population = new Population({ gridMin: -(0.5*gridSize), gridMax: (0.5*gridSize) });
+const population = new Population({
+  gridMin: -(0.5 * gridSize),
+  gridMax: 0.5 * gridSize,
+});
 population.populate(initialPopulation);
 const creatures = population.getAllCreatures();
 canvas.updateCanvas(canvasSize, gridSize, Array.from(creatures.values()));
 
-do {
-  const creatures = population.getAllCreatures();
-  creatures.forEach(async creature => {
+for (let i = 0; i < roundRepeat; i++) {
+  creatures.forEach(async (creature) => {
     await creature.act();
-    canvas.updateCanvas(canvasSize, gridSize, Array.from(creatures.values()));
-  })
-} while (false);
+  });
+  canvas.updateCanvas(canvasSize, gridSize, Array.from(creatures.values()));
+  await timer(3000);
+}
