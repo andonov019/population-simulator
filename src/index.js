3be9cf1fde5b9476332502d5af5653a1b6001973
Christoph Riedlberger <christoph.riedlberger@avenga.com>
2023-09-22 18:54:49 +0200

    Update Canvas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import { Canvas } from "./js/canvas.js";
 
-const canvas = new Canvas(10);
+const canvas = new Canvas(500, 10);
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,16 +1,26 @@
 export class Canvas {
-  constructor(cellSlots) {
-    this.renderCanvas(cellSlots);
+  constructor(size, boxes) {
+    this.renderCanvas(size, boxes);
   }
 
-  renderCanvas(cellsToGenerate) {
-    const container = document.getElementById("grid-container");
+  renderCanvas(size, boxes) {
+    const canvas = document.getElementById("grid-container");
+    canvas.width = size;
+    canvas.height = size;
+    const ctx = canvas.getContext("2d");
+    const boxSize = size / (boxes + 1) -1;
 
-    for (let i = 0; i < cellsToGenerate; i++) {
-      const newDiv = document.createElement("div");
-      newDiv.classList.add("grid-cell");
-      newDiv.textContent = i;
-      container.appendChild(newDiv);
+    for (let i = 0.5*boxes; i >= -0.5*boxes; i--)
+      for (let j = -0.5*boxes; j <= 0.5*boxes; j++)  {
+
+        let x =  (0.5 * size - 0.5 * boxSize) + (size / (boxes + 1) * i);
+        let y =  (0.5 * size - 0.5 * boxSize) + (size / (boxes + 1) * j);
+
+        ctx.fillStyle = "#AAAAAA";
+        ctx.fillRect(x, y, boxSize, boxSize);
+        ctx.font = "10px Comic Sans MS";
+        ctx.fillStyle = "black";
+        ctx.fillText(i + ", " + j, x+0.2*boxSize, y+0.5*boxSize);
     }
   }
 }
