e9ae48f78bd98c44a5309a5a5792182539f098f0
Arash Fallahi <ali.fallahi@avenga.com>
2023-09-22 22:12:45 +0200

    add population to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { Canvas } from "./js/canvas.js";
+import { Population } from "./collections/population.collection.js";
 
 const canvas = new Canvas(500, 10);
+const population = new Population({ gridMin: 0, gridMax: 10 });
